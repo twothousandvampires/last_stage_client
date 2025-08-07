87321535489c85ec9c20da213b654eb28f8f5d09
Your Name <you@example.com>
2025-08-07 17:55:39 +0300

    add config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js"
 import Client from "./scripts/Client"
+import url from "../config"
 
-const socket = io.connect('http://localhost:9001')
+const socket = io.connect(url)
 
 socket.on('connect', () => {
     new Client(socket)
