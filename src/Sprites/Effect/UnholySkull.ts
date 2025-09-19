873b741987183295e15eb5f33747e2b5ea65eb1c
Your Name <you@example.com>
2025-09-19 16:44:50 +0300

    fixes

--- src/Sprites/Effect/UnholySkull.ts
+++ src/Sprites/Effect/UnholySkull.ts
@@ -4,7 +4,7 @@ export default class UnholySkull extends Sprite {
     constructor(id) {
         super(id);
         this.max_frame = 5;
-        this.max_frame_tick = 2;
+        this.max_frame_tick = 4;
         this.sprite_w = 40;
         this.sprite_h = 40;
         this.sprite_name = 'pack3';
